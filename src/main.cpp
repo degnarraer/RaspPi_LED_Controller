6e148e434c0eb61286fc6d189837be64a7246700
Rob <degnarraer@yahoo.com>
2025-03-10 16:02:27 -0500

    Adding microphone callback

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 #include "signal.h"
 #include "i2s_microphone.h"
 
+void Microphone_Callback(const std::vector<int32_t>& data)
+{
+    std::cout << "Callback!" << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     I2SMicrophone mic = I2SMicrophone("Microphone", 44100, 2, 100);
+    mic.StartReading(Microphone_Callback);
     return 0;
-} 
+}
